1a8a3f5aeb0272a75b5a9fbbcccd2d38cc604a90
Pedro Place <pedro.place@hotmail.com>
2024-10-11 16:09:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 name = input('Qual é o teu nome? ')
-idade = input('Qual é a sua idede? ')
+idade = input('Qual é a sua idade? ')
 consulta = input('Qual é a sua consulta? ')
 data = input('Quando vai decorrer a consulta? ')
 print(f'{name} com {idade} anos vai ter consulta de {consulta} no dia {data}')
