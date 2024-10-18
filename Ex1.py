741bbf1a461cf6c17631bca7585517dad44a2b72
Pedro Place <pedro.place@hotmail.com>
2024-10-18 11:36:37 +0100

    Create Ex1.py

---
+++ Ex1.py
@@ -0,0 +1,7 @@
+users, pwds, genders = [], [], []
+for user in range(2):
+    users += [input('Username: ')]
+    pwds += [input('Password: ')]
+    genders += [input('Geders: ')]
+    print('\nUtilizador Registrado\n----------\n')
+print(len(users))
