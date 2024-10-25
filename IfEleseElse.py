6d6804445d45bb0fda123572efdc009b8c7fadfa
Pedro Place <pedro.place@hotmail.com>
2024-10-25 10:00:19 +0100

    Create IfEleseElse.py

---
+++ IfEleseElse.py
@@ -0,0 +1,7 @@
+#Statements if, elif e else
+users = ["Tiago", "Ana"]
+for user in users:
+    if user == "Tiago":
+        print(f"Bom dia, meu amigo {user}.")
+    else:
+        print(f"Bom dia, minha amiga {user}.")
