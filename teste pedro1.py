84aa008b8652c136cd2ff097f7401bfb00aa5f3e
Pedro Place <pedro.place@hotmail.com>
2024-10-25 10:00:58 +0100

    Create teste pedro1.py

---
+++ teste pedro1.py
@@ -0,0 +1,13 @@
+#
+# Teste código dado em aula
+a=2
+b=5
+c=(a+b)/2
+print(f"{a} mais {b} = {a+b}")
+print(f"{a} menos {b} = {a-b}")
+print(f"{a} vezes {b} = {a*b}")
+print(f"{a} a dividir {b} = {a/b}")
+print(f"resto de {a} a dividir {b} = {a%b}")
+print(f"{a} divisao inteira por {b} = {a//b}")
+print(f"{a} elevado {b} = {a**b}")
+print(f"{c}")
