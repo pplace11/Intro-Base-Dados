8b0be8661c435a347a9e8df4deff096bc94c77bb
Pedro Place <pedro.place@hotmail.com>
2024-10-25 09:59:36 +0100

    Create Carro.py

---
+++ Carro.py
@@ -0,0 +1,42 @@
+import sqlite3
+conexao=sqlite3.connect('carro.db')
+# 'car'
+cursor=conexao.cursor()
+cursor.execute('DROP TABLE IF EXISTS marcas')
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS marcas (
+        id INTEGER PRIMARY KEY,
+        marca TEXT,
+        modelos TEXT
+        )
+    ''')
+cursor.execute('''
+ INSERT INTO marcas(marca, modelos)
+ VALUES('Opel', 'Silverato')
+''')
+
+conexao.commit()
+
+cursor.execute('SELECT * FROM marcas')
+resultado = cursor.fetchall()
+for linha in resultado:
+    print(linha)
+# 'pessoas'
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS comprador(
+        id INTEGER PRIMARY KEY,
+        nome TEXT,
+        valor INTEGER,
+        mes TEXT
+        )
+    ''')
+cursor.execute('''
+    INSERT INTO comprador(nome, valor, mes)
+    VALUES('Pedro', 30000, 'Novembro')
+    ''')
+conexao.commit()
+cursor.execute('SELECT * FROM comprador')
+resultado = cursor.fetchall()
+for linha in resultado:
+    print(linha)
+conexao.close()
