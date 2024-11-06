47638fdc48a9d3dc9d018231abb61ab9adc95d9b
Pedro Place <pedro.place@hotmail.com>
2024-11-06 20:15:52 +0000

    Update main4.py

--- main4.py
+++ main4.py
@@ -7,8 +7,25 @@ CREATE TABLE IF NOT EXISTS clientes (
     id INTEGER PRIMARY KEY,
     nome TEXT,
     idade INTEGER
-)
-''')
+)''')
+cursor.execute(''' DROP TABLE IF EXISTS produtos ''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS produtos (
+    id INTEGER PRIMARY KEY,
+    nome TEXT,
+    preco REAL
+)''')
+cursor.execute(''' DROP TABLE IF EXISTS compras ''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS compras (
+    id INTEGER PRIMARY KEY,
+    cliente_id INTEGER,
+    produto_id INTEGER,
+    quantidade INTEGER,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+    FOREIGN KEY (produto_id) REFERENCES produtos(id)
+)''')
+
 cursor.execute('''
 INSERT INTO clientes (nome, idade) VALUES ('Pedro Place', 22)
 ''')
@@ -17,20 +34,6 @@ INSERT INTO clientes (nome, idade) VALUES ('Bruna Silva', 23)
 ''')
 cursor.execute('''
 INSERT INTO clientes (nome, idade) VALUES ('Matilde Braga', 25)
-''')
-conexao.commit()
-cursor.execute('SELECT * FROM clientes')
-resultados = cursor.fetchall()
-for linha in resultados:
-    print(linha)
-print( )
-cursor.execute(''' DROP TABLE IF EXISTS produtos ''')
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS produtos (
-    id INTEGER PRIMARY KEY,
-    nome TEXT,
-    preco REAL
-)
 ''')
 cursor.execute('''
 INSERT INTO produtos (nome, preco) VALUES ('Caneta', 1.50)
@@ -41,23 +44,6 @@ INSERT INTO produtos (nome, preco) VALUES ('Lapis', 1.40)
 cursor.execute('''
 INSERT INTO produtos (nome, preco) VALUES ('Caderno', 3.75)
 ''')
-conexao.commit()
-cursor.execute('SELECT * FROM produtos')
-resultados = cursor.fetchall()
-for linha in resultados:
-    print(linha)
-print( )
-cursor.execute(''' DROP TABLE IF EXISTS compras ''')
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS compras (
-    id INTEGER PRIMARY KEY,
-    cliente_id INTEGER,
-    produto_id INTEGER,
-    quantidade INTEGER,
-    FOREIGN KEY (cliente_id) REFERENCES clientes(id),
-    FOREIGN KEY (produto_id) REFERENCES produtos(id)
-)
-''')
 cursor.execute('''
 INSERT INTO compras (cliente_id, produto_id, quantidade) VALUES (1, 1, 2)
 ''')
@@ -67,17 +53,6 @@ INSERT INTO compras (cliente_id, produto_id, quantidade) VALUES (2, 2, 1)
 cursor.execute('''
 INSERT INTO compras (cliente_id, produto_id, quantidade) VALUES (3, 3, 3)
 ''')
-conexao.commit()
-cursor.execute('''
-SELECT clientes.nome, produtos.nome, compras.quantidade
-FROM compras
-JOIN clientes ON compras.cliente_id = clientes.id
-JOIN produtos ON compras.produto_id = produtos.id
-''')
-resultados = cursor.fetchall()
-for linha in resultados:
-    print(f"Cliente: {linha[0]}, Produto: {linha[1]}, Quantidade: {linha[2]}")
-print( )
 cursor.execute('''
 ALTER TABLE compras ADD COLUMN data DATE
 ''')
@@ -94,6 +69,27 @@ cursor.execute('''
 INSERT INTO compras (cliente_id, produto_id, quantidade, data) VALUES (1, 3, 1, '2023-11-05')
 ''')
 conexao.commit()
+
+cursor.execute('SELECT * FROM clientes')
+resultados = cursor.fetchall()
+for linha in resultados:
+    print(linha)
+print( )
+cursor.execute('SELECT * FROM produtos')
+resultados = cursor.fetchall()
+for linha in resultados:
+    print(linha)
+print( )
+cursor.execute('''
+SELECT clientes.nome, produtos.nome, compras.quantidade
+FROM compras
+JOIN clientes ON compras.cliente_id = clientes.id
+JOIN produtos ON compras.produto_id = produtos.id
+''')
+resultados = cursor.fetchall()
+for linha in resultados:
+    print(f"Cliente: {linha[0]}, Produto: {linha[1]}, Quantidade: {linha[2]}")
+print( )
 cursor.execute('''
 SELECT clientes.nome, produtos.nome, compras.data
 FROM compras
